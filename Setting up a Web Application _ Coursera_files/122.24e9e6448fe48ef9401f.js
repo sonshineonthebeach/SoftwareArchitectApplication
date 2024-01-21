b125ae449afab345577b1cabd3f55874fede3dd8
sonshineonthebeach <sole4482@colorado.edu>
2024-01-20 19:10:48 -0700

    add header and info above input box

---
+++ Setting up a Web Application _ Coursera_files/122.24e9e6448fe48ef9401f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[122],{JISx:function(module,exports){Date.now()}}]);
+//# sourceMappingURL=122.24e9e6448fe48ef9401f.js.map
---
+++ Setting up a Web Application _ Coursera_files/allStyles.ef77619f99d0ddf7ca7c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([["allStyles"],[]]);
+//# sourceMappingURL=allStyles.ef77619f99d0ddf7ca7c.js.map
